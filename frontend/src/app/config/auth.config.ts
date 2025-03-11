87109c02a2858b809598179a913c9019fb71cebf
maksKud <makssoccer@gmail.com>
2025-03-11 13:40:52 +0300

    fix bugs

--- frontend/src/app/config/auth.config.ts
+++ frontend/src/app/config/auth.config.ts
@@ -3,9 +3,9 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 export const authConfig: PassedInitialConfig = {
   config: {
     // версия для без kubernetes
-    // authority: 'http://localhost:8181/realms/spring-microservice-security-realm',
+    // authority: 'http://localhost:8181/realms/spring-microservices-security-realm',
     // Версия для Штурвала
-    authority: 'http://keycloak.default.svc.cluster.local:8080/realms/spring-microservice-security-realm',
+    authority: 'http://keycloak.default.svc.cluster.localhost:8181/realms/spring-microservices-security-realm',
     redirectUrl: window.location.origin,
     postLogoutRedirectUri: window.location.origin,
     clientId: 'angular-client',
--- k8s/kind/kind-load.sh
+++ k8s/kind/kind-load.sh
@@ -4,7 +4,6 @@ docker pull confluentinc/cp-zookeeper:7.5.0
 docker pull confluentinc/cp-kafka:7.5.0
 docker pull confluentinc/cp-schema-registry:7.5.0
 docker pull provectuslabs/kafka-ui:latest
-docker pull mysql:8
 docker pull quay.io/keycloak/keycloak:24.0.1
 docker pull mailhog/mailhog:latest
 docker pull grafana/loki:main
@@ -13,9 +12,10 @@ docker pull grafana/tempo:2.2.2
 docker pull grafana/grafana:10.1.0
 docker pull makskud/api-gateway:0.0.1-SNAPSHOT
 docker pull makskud/product-service:0.0.1-SNAPSHOT
-docker pull makskud/norder-service:0.0.1-SNAPSHOT
+docker pull makskud/order-service:0.0.1-SNAPSHOT
 docker pull makskud/inventory-service:0.0.1-SNAPSHOT
 docker pull makskud/notification-service:0.0.1-SNAPSHOT
+docker pull makskud/frontend:latest
 
 kind load docker-image -n microservices mongo:7.0.5
 kind load docker-image -n microservices postgres:15
@@ -23,7 +23,6 @@ kind load docker-image -n microservices confluentinc/cp-zookeeper:7.5.0
 kind load docker-image -n microservices confluentinc/cp-kafka:7.5.0
 kind load docker-image -n microservices confluentinc/cp-schema-registry:7.5.0
 kind load docker-image -n microservices provectuslabs/kafka-ui:latest
-kind load docker-image -n microservices mysql:8
 kind load docker-image -n microservices quay.io/keycloak/keycloak:24.0.1
 kind load docker-image -n mailhog/mailhog:latest
 kind load docker-image -n microservices grafana/loki:main
@@ -35,3 +34,11 @@ kind load docker-image -n microservices makskud/product-service:0.0.1-SNAPSHOT
 kind load docker-image -n microservices makskud/order-service:0.0.1-SNAPSHOT
 kind load docker-image -n microservices makskud/inventory-service:0.0.1-SNAPSHOT
 kind load docker-image -n microservices makskud/notification-service:0.0.1-SNAPSHOT
+kind load docker-image -n microservices makskud/frontend:latest
+
+#kind rollout restart deployment -n microservices makskud/api-gateway:0.0.2-SNAPSHOT
+#kind rollout restart deployment -n microservices makskud/product-service:0.0.1-SNAPSHOT
+#kind rollout restart deployment -n microservices makskud/order-service:0.0.2-SNAPSHOT
+#kind rollout restart deployment -n microservices makskud/inventory-service:0.0.2-SNAPSHOT
+#kind rollout restart deployment -n microservices makskud/notification-service:0.0.2-SNAPSHOT
+#kind rollout restart deployment -n microservices makskud/frontend:latest
